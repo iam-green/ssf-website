5cfd5614bc6ce02bdc8561230e85739e9a8a0af1
Yun Do Hyeon <33440293+F1N2@users.noreply.github.com>
2022-08-13 10:46:06 +0900

    Update facebook.tsx

--- pages/facebook.tsx
+++ pages/facebook.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next"
 const Facebook: NextPage = () => {
     return (
         <script>
-            window.location.href=`https://www.facebook.com/2022%EC%84%A0%EB%A6%B0%EC%86%8C%ED%94%84%ED%8A%B8%EC%9B%A8%EC%96%B4%EB%82%98%EB%88%94%EC%B6%95%EC%A0%9C-106672018817669`;
+            window.location.href=`https://www.facebook.com/SSFKorea`;
         </script>
     )
 }
